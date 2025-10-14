883dc7b486051f1dc4b6ba2f03b8c66d171a80a0
AshvathGupta <ashvathgupta9@gmail.com>
2025-10-13 20:27:37 -0700

    W auto needs some .polarpath files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRobot.java
@@ -5,10 +5,15 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.Commands.CommandIntake;
+import org.firstinspires.ftc.teamcode.Commands.CommandShoot;
 import org.firstinspires.ftc.teamcode.Commands.SequentialCommandGroup;
 
 import org.firstinspires.ftc.teamcode.PathingTool.PathLoading;
 
+import org.firstinspires.ftc.teamcode.Subsystems.AprilTagState;
+import org.firstinspires.ftc.teamcode.Subsystems.IntakeState;
+import org.firstinspires.ftc.teamcode.Subsystems.ShooterState;
 import org.firstinspires.ftc.teamcode.Tools.NewRobot;
 import org.firstinspires.ftc.teamcode.Commands.CommandScheduler;
 import org.firstinspires.ftc.teamcode.PathingTool.PolarPathFollower;
@@ -19,39 +24,66 @@ import org.firstinspires.ftc.teamcode.Tools.Constants;
 import org.firstinspires.ftc.teamcode.Tools.FieldOfMerit;
 import org.firstinspires.ftc.teamcode.Tools.FinalPose;
 import org.firstinspires.ftc.teamcode.Tools.Mouse;
-
 @Autonomous
 public class AutoRobot extends LinearOpMode {
-
     private FtcDashboard dashboard;
-
+    private AprilTagState aprilTagState = new AprilTagState("aprilStates");
+    private ShooterState shooterState = new ShooterState("shooterStates");
+    private IntakeState intakeState = new IntakeState("intakeStates");
     @Override
     public void runOpMode() throws InterruptedException {
+
+        intakeState.init(hardwareMap);
+        shooterState.init(hardwareMap);
+        aprilTagState.init(hardwareMap);
+
         dashboard = FtcDashboard.getInstance();
+
         FieldOfMerit.initialize(hardwareMap);
+
         Mouse.init(hardwareMap);
+
         Mouse.configureOtos();
+
+        CommandScheduler scheduler = new CommandScheduler();
+
         NewRobot robot = new NewRobot(hardwareMap);
+        robot.aprilTagState = aprilTagState;
+        robot.shooterStates = shooterState;
+        robot.intakeStates = intakeState;
+        scheduler.setNewRobot(robot);
+
         Drive drive = new Drive("drive", hardwareMap);
+
         drive.setPosition(0, 0, 0);
 
-        PathLoading path1 = new PathLoading(hardwareMap.appContext, "DriveForwardOneMeter.polarpath");
-        CommandScheduler scheduler = new CommandScheduler();
+        PathLoading path1 = new PathLoading(hardwareMap.appContext, "");
+        PathLoading path2 = new PathLoading(hardwareMap.appContext, "");
+
         drive = new Drive("drive", hardwareMap);
         Peripherals peripherals = new Peripherals("peripherals");
+
         PolarPathFollower AutoRobot1;
+        PolarPathFollower AutoRobot2;
 
 
         waitForStart();
         try {
+
             AutoRobot1 = new PolarPathFollower(drive, peripherals, path1.getJsonPathData(), Constants.commandMap, Constants.conditionMap, scheduler);
+            AutoRobot2 = new PolarPathFollower(drive, peripherals, path2.getJsonPathData(), Constants.commandMap, Constants.conditionMap, scheduler);
+
             scheduler.schedule(new SequentialCommandGroup(scheduler,
-                    AutoRobot1
+                    AutoRobot1,
+                    new CommandIntake(robot.intakeStates),
+                    AutoRobot2,
+                    new CommandShoot(robot.shooterStates, 5)
             ));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
         while (opModeIsActive()) {
+
             FinalPose.poseUpdate();
 
             scheduler.run();
