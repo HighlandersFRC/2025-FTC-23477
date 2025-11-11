25bf28f9fa4903e3bc2f53eaf8eb7c0807c08aab
averyste207-creator <averyste207@gmail.com>
2025-11-10 18:06:12 -0700

    limeLightTurner.java works with accuracy, however the PID isn't fully tuned. limeLightDriver.java has drive code implemented.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limeLightDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limeLightDriver.java
@@ -7,20 +7,20 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.Tools.PID;
+import org.firstinspires.ftc.teamcode.Tools.SparkFunOTOS;
+
 import org.firstinspires.ftc.teamcode.Commands.CommandIntake;
+import org.firstinspires.ftc.teamcode.Commands.CommandOuttake;
 import org.firstinspires.ftc.teamcode.Commands.CommandScheduler;
+import org.firstinspires.ftc.teamcode.Commands.CommandShoot;
 import org.firstinspires.ftc.teamcode.Commands.ConditionalCommand;
 import org.firstinspires.ftc.teamcode.Commands.ParallelCommandGroup;
-import org.firstinspires.ftc.teamcode.Commands.CommandOuttake;
-import org.firstinspires.ftc.teamcode.Commands.CommandShoot;
-
 import org.firstinspires.ftc.teamcode.Subsystems.Drive;
 import org.firstinspires.ftc.teamcode.Subsystems.IntakeState;
 import org.firstinspires.ftc.teamcode.Subsystems.ShooterState;
-
 import org.firstinspires.ftc.teamcode.Tools.NewRobot;
 import org.firstinspires.ftc.teamcode.Tools.Parameters;
-import org.firstinspires.ftc.teamcode.Tools.PID;
 
 import java.util.List;
 
@@ -33,131 +33,129 @@ public class limeLightDriver extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        // Initialize intake and shooter states
-        intakeStates.init(hardwareMap);
-        shooterState.init(hardwareMap);
+        // Vision hardware
+        Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(0);
+        limelight.start();
 
-        // Initialize robot with intake and shooter states
-        NewRobot robot = new NewRobot(hardwareMap);
-        robot.intakeStates = intakeStates;
-        robot.shooterStates = shooterState;
-        scheduler.setNewRobot(robot);
+        SparkFunOTOS mouse = hardwareMap.get(SparkFunOTOS.class, "mouse");
+        mouse.begin();
+        mouse.resetTracking();
+        mouse.setAngularUnit(SparkFunOTOS.AngularUnit.DEGREES);
 
-        // Initialize drive subsystem
-        Drive drive = new Drive("drive", hardwareMap);
-
-        // Initialize motors for vision control
+        // Initialize motors for manual vision-assisted driving
         DcMotor right_front = hardwareMap.get(DcMotor.class, "right_front");
         DcMotor left_front = hardwareMap.get(DcMotor.class, "left_front");
         DcMotor right_back = hardwareMap.get(DcMotor.class, "right_back");
         DcMotor left_back = hardwareMap.get(DcMotor.class, "left_back");
 
-        // Reset and prepare motors for encoder usage
         right_front.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        right_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        left_front.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        left_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
         right_back.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        right_back.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        left_front.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         left_back.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        right_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        right_back.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        left_front.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         left_back.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        // Initialize Limelight
-        Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
-        limelight.pipelineSwitch(0);
-        limelight.start();
+        // Command and subsystem robot structure
+        intakeStates.init(hardwareMap);
+        shooterState.init(hardwareMap);
 
-        // PID controllers for vision alignment
-        PID pidmt = new PID(0.04, 0.0, 0.0);
-        PID pidrotx = new PID(0.04, 0.0, 0.0);
-        PID pidroty = new PID(0.04, 0.0, 0.0);
+        NewRobot robot = new NewRobot(hardwareMap);
+        robot.intakeStates = intakeStates;
+        robot.shooterStates = shooterState;
+        scheduler.setNewRobot(robot);
 
+        Drive drive = new Drive("drive", hardwareMap);
+
+        // Vision control toggle
         waitForStart();
-
         while (opModeIsActive()) {
-            // Periodic updates for intake and shooter states
             intakeStates.periodic();
             shooterState.periodic();
 
-            // Gamepad controls for commands (intake, shoot, outtake)
-            if (gamepad1.right_trigger > 0) {
-                scheduler.schedule(new CommandIntake(robot.intakeStates, 150));
-            } else if (gamepad1.right_bumper) {
-                scheduler.schedule(new ConditionalCommand(
-                        new ParallelCommandGroup(
-                                scheduler, Parameters.ALL,
-                                new CommandShoot(robot.shooterStates, 5000, 10000, 2500, true),
-                                new CommandIntake(robot.intakeStates, 2500)
-                        ),
-                        new CommandShoot(robot.shooterStates, 5000, 10000, 10000, true),
-                        () -> robot.shooterStates.isAtTargetVelocity()
-                ));
-            } else if (gamepad1.left_trigger > 0) {
-                scheduler.schedule(new CommandOuttake(robot.intakeStates, 1000));
-            }
+            // ------ VISION-ASSISTED DRIVE ------
+            {
+                LLResult result = limelight.getLatestResult();
+                double tx = 0;
+                int id = 0;
+                if (result != null && result.isValid()) {
+                    List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
+                    int targetCount = fiducials.size();
 
-            // Run the scheduler
-            scheduler.run();
+                    tx = result.getTx();
+                    id = targetCount > 0 ? fiducials.get(0).getFiducialId() : -1;
+                    int error = 1;
+                    PID theta = new PID(1.0, 0.0, 0.02);
 
-            // Drive control - field centric drive with gamepad
-            drive.FieldCentric(gamepad1);
+                    double rotxfactor = (tx / 99.9 / 1440);
+                    double mouserot = mouse.getAngularScalar();
+                    double motorpowerdivid = (theta.updatePID(rotxfactor + mouserot) / 2);
+                    double power = motorpowerdivid / 2;
+                    double bx = result.getBotpose().getPosition().x;
+                    double by = result.getBotpose().getPosition().y;
 
-            // Vision processing for turning based on Limelight input
-            LLResult result = limelight.getLatestResult();
-            if (result != null && result.isValid()) {
-                List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
-                double tx = result.getTx();
-                double ty = result.getTy();
-                double bx = result.getBotpose().getPosition().x;
-                double by = result.getBotpose().getPosition().y;
-
-                double avgmotopos = (right_back.getCurrentPosition() + right_front.getCurrentPosition() + left_back.getCurrentPosition() + left_front.getCurrentPosition());
-                double motorpowerdivid = (pidmt.updatePID(avgmotopos) + pidrotx.updatePID(tx) + pidroty.updatePID(ty));
-                double power = Math.max(-0.4, Math.abs(motorpowerdivid));
-
-                int error = 1;
-                int targetCount = fiducials.size();
-
-                if (targetCount > 0) {
-                    int id = fiducials.get(0).getFiducialId();
-
-                    if (tx < -error && id == 24) {
-                        right_front.setPower(-power);
-                        left_front.setPower(-power);
-                        right_back.setPower(power);
-                        left_back.setPower(power);
-                    } else if (tx > error && id == 24) {
-                        right_front.setPower(power);
-                        left_front.setPower(power);
-                        right_back.setPower(-power);
-                        left_back.setPower(-power);
-                    } else {
-                        // Stop motors if aligned
+                    if (id == 24 && gamepad1.a) {
+                        if (tx < -error) {
+                            // turn left
+                            right_front.setPower(power);
+                            left_front.setPower(power);
+                            right_back.setPower(-power);
+                            left_back.setPower(-power);
+                        } else if (tx > error) {
+                            // turn right
+                            right_front.setPower(-power);
+                            left_front.setPower(-power);
+                            right_back.setPower(power);
+                            left_back.setPower(power);
+                        } else {
+                            // localized
+                            right_front.setPower(0);
+                            left_front.setPower(0);
+                            right_back.setPower(0);
+                            left_back.setPower(0);
+                            telemetry.addData("status", "localized");
+                        }
+                    } else if (targetCount == 0) {
+                        // No AprilTag - stop motors
                         right_front.setPower(0);
                         left_front.setPower(0);
                         right_back.setPower(0);
                         left_back.setPower(0);
+                        telemetry.addData("status", "no tag");
                     }
                 } else {
-                    // Stop motors if no target
-                    right_front.setPower(0);
-                    left_front.setPower(0);
-                    right_back.setPower(0);
-                    left_back.setPower(0);
+                    sleep(0);
                 }
 
-                // Telemetry for driver station
-                telemetry.addData("yaw", result.getBotpose().getOrientation().getYaw());
-                telemetry.addData("pitch", ty);
-                telemetry.addData("coordinates", "x: " + bx + " y: " + by);
+                if (gamepad1.right_trigger > 0) {
+                    scheduler.schedule(new CommandIntake(robot.intakeStates, 150));
+                } else if (gamepad1.right_bumper) {
+                    scheduler.schedule(
+                            new ConditionalCommand(
+                                    new ParallelCommandGroup(
+                                            scheduler, Parameters.ALL,
+                                            new CommandShoot(robot.shooterStates, 5000, 10000, 2500, true),
+                                            new CommandIntake(robot.intakeStates, 2500)
+                                    ),
+                                    new CommandShoot(robot.shooterStates, 5000, 10000, 10000, true),
+                                    () -> robot.shooterStates.isAtTargetVelocity()
+                            )
+                    );
+                } else if (gamepad1.left_trigger > 0) {
+                    scheduler.schedule(new CommandOuttake(robot.intakeStates, 1000));
+                }
+
+                scheduler.run();
+                drive.FieldCentric(gamepad1);
+
                 telemetry.addData("tx", tx);
-                telemetry.addData("id", (targetCount > 0) ? fiducials.get(0).getFiducialId() : "None");
-                telemetry.addData("family", (targetCount > 0) ? fiducials.get(0).getFamily() : "None");
+                telemetry.addData("id", id);
                 telemetry.update();
             }
         }
     }
 }
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limeLightTurner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limeLightTurner.java
@@ -7,7 +7,9 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
 import org.firstinspires.ftc.teamcode.Tools.PID;
+import org.firstinspires.ftc.teamcode.Tools.SparkFunOTOS;
 
 import java.util.List;
 
@@ -49,6 +51,12 @@ public class limeLightTurner extends LinearOpMode {
         left_back.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         left_back.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        SparkFunOTOS mouse;
+        mouse = hardwareMap.get(SparkFunOTOS.class, "mouse");
+        mouse.begin();
+        mouse.resetTracking();
+        mouse.setAngularUnit(SparkFunOTOS.AngularUnit.DEGREES);
+
         //starts the code
         waitForStart();
         while (opModeIsActive()) {
@@ -64,20 +72,23 @@ public class limeLightTurner extends LinearOpMode {
                 double ty = result.getTy();
                 double bx = result.getBotpose().getPosition().x;
                 double by = result.getBotpose().getPosition().y;
+                double rot = result.getBotpose().getOrientation().getYaw();
+                double xmo = mouse.getPosition().x;
+                double ymo = mouse.getPosition().y;
+                double botx = ((xmo + bx)/2);
+                double boty = ((ymo + by)/2);
+                double revfactor = 99.9;
+                double mouserot = mouse.getAngularScalar();
 
-                //initializes pid
-                PID pidmt = new PID(0.04, 0.0, 0.0);
-                PID pidrotx = new PID(0.04, 0.0, 0.0);
-                PID pidroty = new PID(0.04, 0.0, 0.0);
-
+                //initializes pids
+                //PID pidmt = new PID(0.04, 0.0, 0.01);
+                PID theta = new PID(1, 0.0, 0.02);
 
                 //continues double initialization
-
-
+                double rotxfactor = (tx/99.9/1440);
+                double tarpos = result.getFiducialResults().get(0).getTargetPoseCameraSpace().getPosition().x;
                 double avgmotopos = (right_back.getCurrentPosition() + right_front.getCurrentPosition() + left_back.getCurrentPosition() + left_front.getCurrentPosition());
-                double motorpowerdivid = (pidmt.updatePID(avgmotopos) + pidrotx.updatePID(tx) + pidroty.updatePID(ty));
-                double rot = result.getBotpose().getOrientation().getYaw();
-
+                double motorpowerdivid = (theta.updatePID(rotxfactor + mouserot)/2);
 
                 //initializes integers
                 int error = 1;
@@ -92,36 +103,51 @@ public class limeLightTurner extends LinearOpMode {
                 String family = result.getFiducialResults().get(0).getFamily();
                 String curpos = String.valueOf(result.getFiducialResults().get(0).getRobotPoseFieldSpace());
                 String robtarpos = String.valueOf(result.getFiducialResults().get(0).getRobotPoseTargetSpace());
-                double power = Math.max(-0.4, Math.abs(motorpowerdivid));
+
+                // initializes just ONE MORE double
+                double power = motorpowerdivid/2;
 
                 if (tx < -error && id == 24) {
-                    right_front.setPower(-power);
-                    left_front.setPower(-power);
-                    right_back.setPower(power);
-                    left_back.setPower(power);
-                } else if (tx > error && id == 24) {
+                    //turn left
                     right_front.setPower(power);
                     left_front.setPower(power);
                     right_back.setPower(-power);
                     left_back.setPower(-power);
-                } else if (targetCount == 0){
+                } else if (tx > error && id == 24) {
+                    //turn right
+                    right_front.setPower(-power);
+                    left_front.setPower(-power);
+                    right_back.setPower(power);
+                    left_back.setPower(power);
+                }
+
+                if (targetCount == 0){
+                    //no tag detected
+                    right_front.setPower(0);
+                    left_front.setPower(0);
+                    right_back.setPower(0);
+                    left_back.setPower(0);
+                } else if (tx <= error && id == 24){
+                    //completely localized
+                    telemetry.addData("status", "localized");
+                    telemetry.update();
                     right_front.setPower(0);
                     left_front.setPower(0);
                     right_back.setPower(0);
                     left_back.setPower(0);
                 } else {
-                    telemetry.addData("status", "localized");
+                    //sus action occurred
+                    sleep(0);
                 }
 
+                System.out.println(tarpos);
+
                 //shows telemetry on the driver station
-                telemetry.addData("yaw", rot);
-                telemetry.addData("pitch", ty);
-                telemetry.addData("coordinates", "x: " + bx + " y: " + by);
+                telemetry.addData("yaw", rotxfactor);
+                telemetry.addData("coordinates", "x: " + botx + " y: " + boty);
                 telemetry.addData("tx", tx);
                 telemetry.addData("id", id);
                 telemetry.addData("family", family);
-                telemetry.addData("Current Position", curpos);
-                telemetry.addData("Target Position in comparison to the robot", robtarpos);
                 telemetry.addData("Current Motor Positions", "Right Front: " + motorposr1 + " Left Front: " + motorposl1 + " Right Back: " + motorposr2 + " Left Back: " + motorposl2);
                 telemetry.update();
             }
