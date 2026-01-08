5806ea45ed0a1701c0cf62924756ea30699c2b46
averyste207-creator <averyste207@gmail.com>
2026-01-07 17:43:03 -0700

    Range Tracking command works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/V2robot.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Commands.CommandScheduler;
+import org.firstinspires.ftc.teamcode.Subsystems.Drive;
+import org.firstinspires.ftc.teamcode.Subsystems.IntakeState;
+import org.firstinspires.ftc.teamcode.Tools.Mouse;
+import org.firstinspires.ftc.teamcode.Tools.NewRobot;
+
+@TeleOp
+public class V2robot extends LinearOpMode {
+
+    IntakeState intakeStates = new IntakeState("aashrithStates");
+    CommandScheduler scheduler = new CommandScheduler();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Drive drive = new Drive("drive", hardwareMap);
+
+        intakeStates.init(hardwareMap);
+
+        NewRobot robot = new NewRobot(hardwareMap);
+        robot.intakeStates = intakeStates;
+        scheduler.setNewRobot(robot);
+
+        if (gamepad1.right_trigger > 0) {
+            intakeStates.setWantedState(IntakeState.INTAKE_STATE.INTAKE);
+        } else if (gamepad1.left_trigger > 0) {
+            intakeStates.setWantedState(IntakeState.INTAKE_STATE.OUTTAKE);
+        } else {
+            intakeStates.setWantedState(IntakeState.INTAKE_STATE.DEFAULT);
+        }
+
+        drive.FeildCentric(gamepad1);
+
+        telemetry.addData("Position", "(%.2f, %.2f, %.1f°)",
+                Mouse.getX(), Mouse.getY(), Math.toDegrees(Mouse.getTheta()));
+        telemetry.addData("Controls", "D-pad Up=Enable Auto, D-pad Down=Manual");
+        telemetry.update();
+    }
+}
